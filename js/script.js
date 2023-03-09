ef5f7d4401f1da57f8b0c522740dd00e72240a4c
Trishna-Nath <t.nathbd@gmail.com>
2023-03-08 19:15:40 -0800

    added script section

--- js/script.js
+++ js/script.js
@@ -2,5 +2,26 @@ const hour = document.getElementById('hour');
 const minute = document.getElementById('minute');
 const second = document.getElementById('second');
 
+const clock = setInterval(function time(){
+ let dateToday  = new Date();
+//  console.log(dateToday);
+let hr = dateToday.getHours();
+let min = dateToday.getMinutes();
+let sec = dateToday.getSeconds();
+// console.log(hr,min,sec);
+if(hr < 10){
+    hr = '0' + hr;
+}
+if(min < 10){
+    hr = '0' + hr;
+}
+if(sec < 10){
+    hr = '0' + hr;
+}
+hour.innerText = hr;
+minute.innerText = min;
+second.innerText = sec;
+},1000) ;
+
 
 
